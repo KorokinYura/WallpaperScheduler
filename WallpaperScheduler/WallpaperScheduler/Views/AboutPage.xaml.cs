ecd89f7285afbc746f249f5ee3c8da0abd3274e0
Yurii Korokin <yurii.korokin@gmail.com>
2019-09-02 22:31:30 +0300

    Update about page appearance.

--- WallpaperScheduler/WallpaperScheduler/Views/AboutPage.xaml.cs
+++ WallpaperScheduler/WallpaperScheduler/Views/AboutPage.xaml.cs
@@ -10,9 +10,61 @@ namespace WallpaperScheduler.Views
     [DesignTimeVisible(false)]
     public partial class AboutPage : ContentPage
     {
+        private bool isAboutExpanded = false;
+        private bool isHowToUseExpanded = false;
+        private bool isPerfomanceExpanded = false;
+
         public AboutPage()
         {
             InitializeComponent();
+        }
+
+        protected async void AboutClicked(object sender, EventArgs e)
+        {
+            if (isAboutExpanded)
+            {
+                await AboutStack.FadeTo(0);
+                AboutStack.IsVisible = !isAboutExpanded;
+            }
+            else
+            {
+                AboutStack.IsVisible = !isAboutExpanded;
+                await AboutStack.FadeTo(1);
+            }
+
+            isAboutExpanded = !isAboutExpanded;
+        }
+
+        protected async void HowToUseClicked(object sender, EventArgs e)
+        {
+            if (isHowToUseExpanded)
+            {
+                await HowToUseStack.FadeTo(0);
+                HowToUseStack.IsVisible = !isHowToUseExpanded;
+            }
+            else
+            {
+                HowToUseStack.IsVisible = !isHowToUseExpanded;
+                await HowToUseStack.FadeTo(1);
+            }
+
+            isHowToUseExpanded = !isHowToUseExpanded;
+        }
+
+        protected async void PerfomanceClicked(object sender, EventArgs e)
+        {
+            if (isPerfomanceExpanded)
+            {
+                await PerfomanceStack.FadeTo(0);
+                PerfomanceStack.IsVisible = !isPerfomanceExpanded;
+            }
+            else
+            {
+                PerfomanceStack.IsVisible = !isPerfomanceExpanded;
+                await PerfomanceStack.FadeTo(1);
+            }
+
+            isPerfomanceExpanded = !isPerfomanceExpanded;
         }
     }
 }
